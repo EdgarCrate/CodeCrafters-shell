49c03eaa712f12701075e70b6572d4cad0e12670
EDGARTOYIN <edgartoyin10@gmail.com>
2026-05-18 21:52:15 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,15 @@ use std::io::{self, Write};
 
 fn main() {
     print!("$ ");
+    let mut user_input = String::new();
+
+    std::io::stdin()
+        .read_line(&mut user_input)
+        .expect("Error while reading user input")
+        .trim()
+        .to_string();
+
+    println!("{user_input}: command not found");
+
     io::stdout().flush().unwrap();
 }
