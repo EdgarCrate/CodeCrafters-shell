a58f2abcee54383e0d67ec11805125a9971cb4b5
EDGARTOYIN <edgartoyin10@gmail.com>
2026-06-27 22:54:01 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -32,14 +32,11 @@ impl Commands {
 fn is_bin_in_path(path_value: OsString, bin_name: String) {
     let list_of_paths = env::split_paths(&path_value);
     for path in list_of_paths.into_iter() {
-        if !path.exists() {
-            continue;
-        }
         for dirs in fs::read_dir(&path).unwrap() {
             let dir_item = dirs.unwrap();
             let dir_item_name = dir_item.file_name().into_string().unwrap();
             if dir_item_name == bin_name {
-                let metadata = fs::metadata(&path).expect("Error while reading metadata");
+                let metadata = fs::metadata(dir_item.path()).expect("Error while reading metadata");
                 let mode = metadata.permissions().mode();
                 let is_executable = (mode & 0o111) != 0;
                 if is_executable {
