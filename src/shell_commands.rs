2dfd7b68be61a268dd0771563eae38ca30d32c94
EDGARTOYIN <edgartoyin10@gmail.com>
2026-06-28 21:38:33 -0700

    codecrafters submit [skip ci]

--- src/shell_commands.rs
+++ src/shell_commands.rs
@@ -64,7 +64,11 @@ impl Commands {
                         .map(|(item, _)| item);
                     if let Some(executable) = directive_item {
                         if Commands::is_file_executable(&executable.path()) {
-                            Some(format!("{} is {}", bin_name, executable.path().display()));
+                            return Some(format!(
+                                "{} is {}",
+                                bin_name,
+                                executable.path().display()
+                            ));
                         }
                     } else {
                         continue;
